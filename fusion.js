58380c3251f920dbb0d0fe532b12d4728854a548
Ronald M. Clifford <roncli@roncli.com>
2017-01-05 05:52:54 -0800

    Update discord, further updates.

--- fusion.js
+++ fusion.js
@@ -2,12 +2,14 @@ var pjson = require("./package.json"),
     settings = require("./settings"),
     db = require("./database"),
     messageParse = /^!([^ ]+)(?: +(.+[^ ]))? *$/,
+    idParse = /^<@([0-9]+)>$/,
 
     Fusion = {},
     tmiCooldown = {},
     discordCooldown = {},
+    players = {},
 
-    tmi, discord, obsDescent, generalChannel;
+    tmi, discord, obsDiscord, generalChannel, eventRole, seasonRole, event;
 
 Fusion.start = (_tmi, _discord) => {
     "use strict";
@@ -42,8 +44,6 @@ Fusion.start = (_tmi, _discord) => {
         
         console.log("Starting up...");
         
-        // TODO: Get players from database.
-        
         // Setup events.
         tmi.on("disconnected", (message) => {
             console.log("DISCONNECTED", message);
@@ -62,8 +62,11 @@ Fusion.start = (_tmi, _discord) => {
         
         discord.on("ready", () => {
             console.log("Discord ready.");
-            obsDescent = discord.guilds.find("name", "The Observatory");
-            generalChannel = obsDescent.channels.find("name", "general");
+            //obsDiscord = discord.guilds.find("name", "The Observatory");
+            obsDiscord = discord.guilds.find("name", "roncli Gaming");
+            generalChannel = obsDiscord.channels.find("name", "general");
+            eventRole = obsDiscord.roles.find("name", "In Current Event");
+            seasonRole = obsDiscord.roles.find("name", "Season 1 Participant");
             
             if (!readied) {
                 readied = true;
@@ -74,14 +77,50 @@ Fusion.start = (_tmi, _discord) => {
         });
         
         discord.on("message", (message) => {
-            if (message.guild && message.guild.name === "The Observatory") {
+            var author;
+
+            if (message.guild && message.guild.name === obsDiscord.name) {
+                author = obsDiscord.members.get(message.author.id);
+                Fusion.discordMessage(author.displayName, author, message.channel, message.content);
+            } else if (!message.guild) {
                 Fusion.discordMessage(message.author.username, message.author, message.channel, message.content);
             }
+            
         });
+
+        discordConnect();
     };
     
     startup();
 };
+
+Fusion.isAdmin = (user) => {
+    return user.username === settings.admin.username && user.discriminator == settings.admin.discriminator;
+};
+
+Fusion.getPlayers = new Promise((resolve, reject) => {
+    db.query("SELECT PlayerID, Name, DiscordID, Rating, RatingDeviation, Volatility from tblPlayer", {}, (err, data) => {
+        if (err) {
+            console.log(err);
+            Fusion.discordQueue("@roncli loading players from the database failed, see the log for details.");
+            reject(err);
+        }
+
+        players = {};
+
+        if (!data[0]) {
+            resolve();
+            return;
+        }
+
+        data[0].forEach((index) => {
+            var player = data[0][index];
+            players[player.DiscordID] = player;
+        });
+
+        resolve();
+    });
+});
 
 Fusion.tmiQueue = (message) => {
     "use strict";
@@ -165,71 +204,322 @@ Fusion.discordMessages = {
     version: (from, user, channel, message) => {
         "use strict";
         
-        if (message || channel.id !== generalChannel.id || discordCooldown.version > new Date()) {
+        if (message || discordCooldown.version > new Date()) {
             return;
         }
         
-        Fusion.discordQueue("FusionBot by roncli, Version " + pjson.version);
+        Fusion.discordQueue("FusionBot by roncli, Version " + pjson.version, channel);
         
         discordCooldown.version = new Date(new Date().getTime() + 60000);
     },
     
     join: (from, user, channel, message) => {
         "use strict";
-        
-        // TODO: Join the next tournament.
+
+        if (message) {
+            return;
+        }
+
+        if (!event) {
+            Fusion.discordQueue("Sorry, " + user + ", but there is no event currently running.", channel);
+            return;
+        }
+
+        if (!event.joinable) {
+            Fusion.discordQueue("Sorry, " + user + ", but this is not an event you can join.", channel);
+            return;
+        }
+
+        if (event.players[user.id] && !event.players[user.id].withdrawn) {
+            Fusion.discordQueue("Sorry, " + user + ", but you have already joined this event.  You can use !withdraw to leave it.", channel);
+            return;
+        }
+
+        if (event.players[user.id]) {
+            delete event.players[user.id].withdrawn;
+        } else {
+            event.players[user.id] = {
+                home: undefined,
+                canHost: true
+            };
+        }
+        user.addRole(eventRole);
+        Fusion.discordQueue("You have been successfully added to the event.  Please use the `!home` command to select a home level, for example, `!home Logic x2`.  I assume you can host games, but if you cannot please issue the `!host` command to toggle this option.", user);
+        Fusion.discordQueue(obsDiscord.members.get(user.id).displayName + " has joined the tournament!", generalChannel);
     },
     
     withdraw: (from, user, channel, message) => {
         "use strict";
-        
-        // TODO: Withdraw from the current tournament.
+
+        if (message) {
+            return;
+        }
+
+        if (!event) {
+            Fusion.discordQueue("Sorry, " + user + ", but there is no event currently running.", channel);
+            return;
+        }
+
+        if (!event.joinable) {
+            Fusion.discordQueue("Sorry, " + user + ", but this is not an event you can withdraw from.", channel);
+            return;
+        }
+
+        if (!event.players[user.id]) {
+            Fusion.discordQueue("Sorry, " + user + ", but you have not yet entered this event.  You can use !join to enter it.", channel);
+            return;
+        }
+
+        if (event.players[user.id].withdrawn) {
+            Fusion.discordQueue("Sorry, " + user + ", but you have have already withdrawn from this event.  You can use !join to re-enter it.", channel);
+            return;
+        }
+
+        event.players[user.id].withdrawn = true;
+        user.removeRole(eventRole);
+        Fusion.discordQueue("You have been successfully withdrawn from the event.  If you wish to return before the end of the event, you may use the `!join` command once again.", user);
+        Fusion.discordQueue(obsDiscord.members.get(user.id).displayName + " has withdrawn from the tournament!", generalChannel);
     },
     
     home: (from, user, channel, message) => {
         "use strict";
         
-        // TODO: Set the home level for the tournament.  Can be changed up until the tournament begins.
+        if (!message) {
+            return;
+        }
+
+        if (!event) {
+            Fusion.discordQueue("Sorry, " + user + ", but there is no event currently running.", channel);
+            return;
+        }
+
+        if (!event.players[user.id]) {
+            if (event.joinable) {
+                Fusion.discordQueue("Sorry, " + user + ", but you first need to `!join` the tournament before selecting a home level.", channel);
+                return;
+            } else {
+                Fusion.discordQueue("Sorry, " + user + ", but you are not entered into this tournament.", channel);
+                return;
+            }
+        }
+
+        if (event.players[user.id].withdrawn) {
+            Fusion.discordQueue("Sorry, " + user + ", but you have withdrawn from the tournament.", channel);
+            return;
+        }
+
+        if ((event.round && event.round > 0) || (event.matches && event.matches.length > 0)) {
+            Fusion.discordQueue("Sorry, " + user + ", but you cannot change your home level after the tournament has started.", channel);
+            return;
+        }
+
+        event.players.home = message;
+        Fusion.discordQueue("You have successfully set your home level to `" + message + "`.  You may change it at any time before the tournament begins.", channel);
+        Fusion.discordQueue(obsDiscord.members.get(user.id).displayName + " has set their home level to `" + message + "`.", channel);
     },
     
     host: (from, user, channel, message) => {
         "use strict";
         
-        // TODO: Toggles whether or not user can host.
+        if (message) {
+            return;
+        }
+
+        if (!event) {
+            Fusion.discordQueue("Sorry, " + user + ", but there is no event currently running.", channel);
+            return;
+        }
+
+        if (!event.players[user.id]) {
+            if (event.joinable) {
+                Fusion.discordQueue("Sorry, " + user + ", but you first need to `!join` the tournament before toggling your ability to host games.", channel);
+                return;
+            } else {
+                Fusion.discordQueue("Sorry, " + user + ", but you are not entered into this tournament.", channel);
+                return;
+            }
+        }
+
+        if (event.players[user.id].withdrawn) {
+            Fusion.discordQueue("Sorry, " + user + ", but you have withdrawn from the tournament.", channel);
+            return;
+        }
+
+        if ((event.round && event.round > 0) || (event.matches && event.matches.length > 0)) {
+            Fusion.discordQueue("Sorry, " + user + ", but you cannot toggle your ability to host games after the tournament has started.", channel);
+            return;
+        }
+
+        event.players.canHost = !event.players.canHost;
+        Fusion.discordQueue("You have successfully toggled " + (event.players.canHost ? "on" : "off") + " your ability to host games.  You may change it at any time before the tournament begins.", user);
+        Fusion.discordQueue(obsDiscord.members.get(user.id).displayName + " has set toggled " + (event.players.canHost ? "on" : "off") + " their ability to host games.", generalChannel);
     },
     
     report: (from, user, channel, message) => {
         "use strict";
         
+        if (!message) {
+            return;
+        }
+
         // TODO: Reports the result of a match.
     },
     
     confirm: (from, user, channel, message) => {
         "use strict";
         
+        if (message) {
+            return;
+        }
+
         // TODO: Confirms the result of a match.
     },
     
     comment: (from, user, channel, message) => {
         "use strict";
         
+        if (!message) {
+            return;
+        }
+
         // TODO: Add a comment to the match.
     },
     
     openevent: (from, user, channel, message) => {
         "use strict";
         
-        // TODO: Admin only, open a new event.
+        if (!Fusion.isAdmin(user) || message) {
+            return;
+        }
+
+        if (event) {
+            Fusion.discordQueue("Sorry, " + user + ", but you must !endevent the previous event first.", channel);
+            return;
+        }
+
+        event = {
+            joinable: true,
+            round: 0,
+            players: {},
+            matches: []
+        }
+
+        Fusion.discordQueue("Hey @everyone, a new tournament has been created.  `!join` the tournament if you'd like to play!", generalChannel);
     },
     
     startevent: (from, user, channel, message) => {
         "use strict";
-        
-        // TODO: Admin only, start the event.
+
+        if (!Fusion.isAdmin(user) || message) {
+            return;
+        }
+
+        if (event) {
+            Fusion.discordQueue("Sorry, " + user + ", but you must !endevent the previous event first.", channel);
+            return;
+        }
+
+        event = {
+            joinable: false,
+            players: {},
+            matches: []
+        }
+    },
+
+    addplayer: (from, user, channel, message) => {
+        "use strict";
+
+        var matches, user;
+
+        if (!Fusion.isAdmin(user) || !message) {
+            return;
+        }
+
+        if (!event) {
+            Fusion.discordQueue("Sorry, " + user + ", but there is no event currently running.", channel);
+            return;
+        }
+
+        // Get the user by ID.
+        matches = idParse.exec(message);
+
+        if (!matches) {
+            Fusion.discordQueue("Sorry, " + user + ", but you must mention the user to add them, try this command on a public channel instead.", channel);
+            return;
+        }
+
+        var addedUser = obsDiscord.members.get(matches[1]);
+
+        if (!addedUser) {
+            Fusion.discordQueue("Sorry, " + user + ", but that person is not part of this Discord server.", channel);
+            return;
+        }
+
+        if (event.players[addedUser.id]) {
+            Fusion.discordQueue("Sorry, " + user + ", but " + addedUser.displayName + " has already joined the event.  You can use !removeplayer to remove them instead.", channel);
+            return;
+        }
+
+        event.players[addedUser.id] = {
+            home: undefined,
+            canHost: true
+        };
+
+        user.addRole(eventRole);
+        Fusion.discordQueue("You have been successfully added " + addedUser.displayName + " to the event.", user);
+        Fusion.discordQueue(from + " has added you to the next event!  Please use the `!home` command to select a home level, for example, `!home Logic x2`.  I assume you can host games, but if you cannot please issue the `!host` command to toggle this option.", addedUser);
+        Fusion.discordQueue(addedUser.displayName + " has joined the tournament!", generalChannel);
+    },
+
+    removeplayer: (from, user, channel, message) => {
+        "use strict";
+
+        if (!Fusion.isAdmin(user) || !message) {
+            return;
+        }
+
+        if (!event) {
+            Fusion.discordQueue("Sorry, " + user + ", but there is no event currently running.", channel);
+            return;
+        }
+
+        // Get the user by ID.
+        matches = idParse.exec(message);
+
+        if (!matches) {
+            Fusion.discordQueue("Sorry, " + user + ", but you must mention the user to remove them, try this command on a public channel instead.", channel);
+            return;
+        }
+
+        var removedUser = obsDiscord.members.get(matches[1]);
+
+        if (!removedUser) {
+            Fusion.discordQueue("Sorry, " + user + ", but that person is not part of this Discord server.", channel);
+            return;
+        }
+
+        if (!event.players[removedUser.id]) {
+            Fusion.discordQueue("Sorry, " + user + ", but " + removedUser.displayName + " is not part of the event.  You can use !removeplayer to remove them instead.", channel);
+            return;
+        }
+
+        event.players[removedUser.id] = {
+            home: undefined,
+            canHost: true
+        };
+
+        user.addRole(eventRole);
+        Fusion.discordQueue("You have been successfully removed " + removedUser.displayName + " from the event.", user);
+        Fusion.discordQueue(from + " has removed you from the next event!  If this is in error, please contact ", addedUser);
+        Fusion.discordQueue(removedUser.displayName + " has been removed from the tournament.", generalChannel);
     },
     
     generateround: (from, user, channel, message) => {
         "use strict";
+        
+        if (!Fusion.isAdmin(user) || message) {
+            return;
+        }
         
         // TODO: Admin only, generate the next round of matches.
     },
@@ -237,11 +527,19 @@ Fusion.discordMessages = {
     creatematch: (from, user, channel, message) => {
         "use strict";
         
+        if (!Fusion.isAdmin(user) || !message) {
+            return;
+        }
+        
         // TODO: Admin only, create a match.
     },
     
     endevent: (from, user, channel, message) => {
         "use strict";
+        
+        if (!Fusion.isAdmin(user) || message) {
+            return;
+        }
         
         // TODO: Admin only, end the event.
     }
